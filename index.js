327d45c8cc53e5c7fd6ba70caef5dcbf421c742c
Matthew Crooks <mfcrooks@gmail.com>
2020-08-05 23:11:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,4 @@
 main.remove()
 const newHeader = document.createElement("h1")
 newHeader.id = "victory"
-newHeader.innerHTML = "Matthew is the champion!";
+newHeader.innerHTML = "<h1>Matthew is the champion! </h1>";
